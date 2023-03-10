4817b5dab1488761c240565d58b44b38d52e89a9
TheresaAk <akinbolatheresa19@gmail.com>
2023-03-10 14:50:03 -0800

    fins

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
- */
-char *_strchr(char *s, char c)
+* _strlen - returns the length of a string
+* @s: string
+* Return: length
+*/
+int _strlen(char *s)
 {
-	int i = 0;
-
-	for (; s[i] >= '\0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
-	return (0);
+int longi = 0;
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+return (longi);
 }
